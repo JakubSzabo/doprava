9ab3a27b276604eea07b55cfb34c9d5377983832
jakubszabo <jakub160@gmail.com>
2024-07-14 20:05:24 +0200

    edit
    edit

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
+import {TopNavComponent} from "../top-nav/top-nav.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    TopNavComponent
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
